e89255135efa5e899ce1c824a40987e6e0d32daa
PalumboN <nahuel.palumbo@gmail.com>
2015-07-23 08:10:21 -0300

    Se instala Bower con Angular

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function (grunt) {
       app: 'app',
       server: 'server',
       views: 'views',
+      root: 'public',
       main: 'main.js'
     },
 
@@ -32,15 +33,95 @@ module.exports = function (grunt) {
       }
     },
 
+    // Open browser
     open: {
       server: {
         url: 'http://localhost:<%= express.options.port %>'
       }
     },
 
+    // Compiles CoffeeScript to JavaScript
+    coffee: {
+      server: {
+        files: [
+          {
+            expand: true,
+            cwd: "<%= yeoman.app %>",
+            src: ["**/*.coffee", "!**/*.spec.coffee"],
+            dest: "<%= yeoman.root %>",
+            ext: ".js"
+          }
+        ]
+      }
+    },
+
+
+    injector: {
+      // Inject application script files into main.html (doesn't include bower)
+      scripts: {
+        options: {
+          transform: function(filePath) {
+            filePath = filePath.replace("/public/", "");
+            return "script(src=\"" + filePath + "\")";
+          },
+          starttag: "// injector:js",
+          endtag: "// endinjector"
+        },
+        files: {
+          "<%= yeoman.app %>/<%= yeoman.views %>/layout.jade": [
+            "<%= yeoman.root %>/**/*.js", 
+            "!<%= yeoman.root %>/app.js", 
+            "!<%= yeoman.root %>/bower_components/**/*.js", 
+            "!<%= yeoman.root %>/**/*.spec.js", 
+            "!<%= yeoman.root %>/**/*.mock.js"
+          ]
+        }
+      },
+
+      // Inject component scss into app.scss
+      sass: {
+        options: {
+          transform: function(filePath) {
+            //filePath = filePath.replace("/client/app/", "");
+            return "@import '" + filePath + "';";
+          },
+          starttag: "// injector",
+          endtag: "// endinjector"
+        },
+        files: {
+          "<%= yeoman.root %>/app.scss": [
+            "<%= yeoman.root %>/**/!(_)*.{scss,sass}", 
+            "!<%= yeoman.root %>/app.{scss,sass}"
+          ]
+        }
+      },
+
+      // Inject component css into main.html
+      css: {
+        options: {
+          transform: function(filePath) {
+            filePath = filePath.replace("/app/", "");
+            return "link(rel=\"stylesheet\" href=\"" + filePath + "\")";
+          },
+          starttag: "// injector:css",
+          endtag: "// endinjector"
+        },
+        files: {
+          "<%= yeoman.app %>/<%= yeoman.views %>/layout.jade": ["<%= yeoman.app %>/**/*.css"]
+        }
+      }
+    },
+
+    // Watch for changes
     watch: {
-      coffeeServer: {
-        files: ['<%= yeoman.server %>/**/*.coffee']
+      coffee: {
+        files: [
+        '<%= yeoman.server %>/**/*.coffee',
+        '<%= yeoman.app %>/**/*.coffee'
+        ],
+        tasks: [
+          "newer:coffee"
+        ]
       },
 
       livereload: {
@@ -68,6 +149,15 @@ module.exports = function (grunt) {
           nospawn: true //Without this option specified express won't be reloaded
         }
       }
+    },
+
+    // Automatically inject Bower components into the app
+    wiredep: {
+      target: {
+        src: "<%= yeoman.app %>/<%= yeoman.views %>/layout.jade",
+        ignorePath: "../../public/",
+        exclude: []
+      }
     }
   });
 
@@ -77,6 +167,9 @@ module.exports = function (grunt) {
     }
 
     grunt.task.run([
+      'coffee',
+      'injector',
+      'wiredep',
       'express:dev',
       'open',
       'watch'
---
+++ app/app.coffee
@@ -0,0 +1,6 @@
+'use strict'
+
+window.app = angular.module 'now-or-never-app', [
+  'ui.router',
+  'ui.bootstrap',
+]
